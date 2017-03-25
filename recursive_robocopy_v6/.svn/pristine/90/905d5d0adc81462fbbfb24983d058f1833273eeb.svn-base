0e2984527e9147c9d92d9aacaf45d35e28c3a951
Chris Larivee <christian.larivee@gmail.com>
2017-03-25 11:51:24 -0300

    initial recursive robocopy add
    initial recursive robocopy add

---
+++ recursive_robocopy_v6/.svn/pristine/90/905d5d0adc81462fbbfb24983d058f1833273eeb.svn-base
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace recursive_robocopy
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            try
+            {
+                // simply pass everything in to the internal class 
+                var recursiveRobocopy = new recursiveRobocopyProgram(args);
+                recursiveRobocopy.startRoboCopy();
+            }
+            catch (Exception ex)
+            {
+                // do nothing and stop
+                return;
+            }
+        }
+    }
+}
---
+++ recursive_robocopy_v6/recursive_robocopy/classes/doRoboCopyState.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace recursive_robocopy.classes
+{
+    /// <summary>
+    ///   Class to hold state info to support threading
+    /// </summary>
+    public class doRoboCopyState
+    {
+        private CountdownEvent cde; // total task countdown
+
+        public CountdownEvent Cde
+        {
+            get { return cde; }
+            set { cde = value; }
+        }
+
+        private CountdownEvent cdeActiveTasks; // active task countdown
+
+        public CountdownEvent CdeActiveTasks
+        {
+            get { return cdeActiveTasks; }
+            set { cdeActiveTasks = value; }
+        }
+
+
+        private bool sleepOnException = false;
+
+        public bool SleepOnException
+        {
+            get { return sleepOnException; }
+            set { sleepOnException = value; }
+        }
+
+        /// <summary>
+        ///   output semaphore - allows control of sysout redirection
+        /// </summary>
+        public Semaphore semOutput { get; set; }
+
+        public doRoboCopyState()
+        {}
+
+        public doRoboCopyState(CountdownEvent cde, CountdownEvent cdeactiveTasks , Semaphore semOutput, bool sleeponexception)
+        {
+            this.cde=cde;
+            this.cdeActiveTasks = cdeactiveTasks;
+            this.semOutput = semOutput;
+            this.sleepOnException=sleeponexception;
+        }
+
+        
+    }
+}
